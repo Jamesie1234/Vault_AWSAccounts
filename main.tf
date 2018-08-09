8bb9dbedbd4a2a518af628ac89d203eba49bacac
Dave Arnold <dja+djaboxx@sonic.net>
2018-08-09 16:51:03 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,50 @@
+//--------------------------------------------------------------------
+// Variables
+variable "aws_account_email" {}
+
+variable "aws_account_name" {}
+
+variable "keybase_user" {}
+//--------------------------------------------------------------------
+// Modules
+module "accounts" {
+  source  = "app.terraform.io/SecurityInDepth-Darnold/accounts/aws"
+  version = "3.2.0"
+
+  aws_account_email = "${var.aws_account_email}"
+  aws_account_name  = "${var.aws_account_name}"
+  keybase_user = "${var.keybase_user}"
+  
+}
+
+output "console_password" {
+  value = "${module.accounts.console_password}"
+}
+
+output "role_name" {
+  value = "${module.accounts.role_name}"
+}
+
+output "account_id" {
+  value = "${module.accounts.account_id}"
+}
+
+output "account_arn" {
+  value = "${module.accounts.account_arn}"
+}
+
+output "vault_ec2_ro_path" {
+  value = "aws-${var.aws_account_name}/creds/ec2_ro"
+}
+
+output "vault_ec2_admin_path" {
+  value = "aws-${var.aws_account_name}/creds/ec2_admin"
+}
+
+output "vault_ec2_admin_policy" {
+  value = "aws-${var.aws_account_name}-admin"
+}
+
+output "vault_ec2_ro_policy" {
+  value = "aws-${var.aws_account_name}-ro"
+}
